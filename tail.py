d391adedc5432a050683fef77d72dcf3edcfae6c
Tero Niemi <talamus@gmail.com>
2018-08-29 14:51:40 +0300

    Added support for glob (*.txt) patterns

--- tail.py
+++ tail.py
@@ -9,13 +9,13 @@ Example:
     import tail
 
     # Create a tail instance:
-    t = tail.Tail('file-to-be-followed', callback=print, sleep=1)
+    t = tail.Tail('logs/*.txt', callback=print, sleep=1)
     # Optionally you can register a callback function and set the sleep time.
 
     # Start tailing:
     t.start()
 
-    # Wait for a key press, and stop trailing:
+    # Wait for a key press, and stop tailing:
     input()
     t.stop() '''
 
@@ -25,55 +25,88 @@ Example:
 import os
 import sys
 import time
+import signal
+import glob
 from threading import Thread
+
+def default_callback(message, filename):
+    print(filename, ':', message, flush=True)
 
 class Tail(Thread):
     ''' Represents a tail command. '''
-    def __init__(self, tailed_file, callback=print, sleep=1):
+    def __init__(self, glob_to_be_tailed, callback=default_callback, sleep=2):
         ''' Initiate a Tail instance.
             Check for file validity, assigns callback function to standard out.
 
             Arguments:
-                tailed_file - File to be followed.
+                glob_to_be_tailed - Glob pattern to be followed.
                 callback - alternative callback function. (Default: print)
-                sleep - time between re-checks. (Default: 1) '''
+                sleep - time between re-checks. (Default: 2) '''
 
-        self.check_file_validity(tailed_file)
-        self.tailed_file = tailed_file
+        self.glob_to_be_tailed = glob_to_be_tailed
         self.callback = callback
         self.sleep = sleep
         self.is_active = True
+        self.positions = {}
         Thread.__init__(self)
 
     def run(self):
-        ''' Do a tail follow. For every new line a callback is called. '''
-        with open(self.tailed_file) as file_:
-            # Go to the end of file
-            file_.seek(0,2)
-            while self.is_active:
-                curr_position = file_.tell()
-                line = file_.readline()
-                if not line:
-                    file_.seek(curr_position)
-                    time.sleep(self.sleep)
-                else:
-                    self.callback(line.rstrip())
+        ''' Do the tailing. For every new line a callback is called. '''
+
+        # Seek existing files and go to the end of the file
+        filenames = glob.glob(self.glob_to_be_tailed)
+        for filename in filenames:
+            print(filename, os.path.getsize(filename))
+            self.positions[filename] = os.path.getsize(filename)
+
+        # get a list of matching filenames, and for each try to seek new lines
+        while self.is_active:
+            positions = {}
+            filenames = glob.glob(self.glob_to_be_tailed)
+            for filename in filenames:
+                try:
+                    with open(filename) as file:
+                        if filename in self.positions:
+                            position = self.positions[filename]
+                        else:
+                            position = 0
+                        file.seek(position)
+                        for line in file:
+                            self.callback(line.rstrip(), filename)
+                        position = file.tell()
+                        file.close()
+                    positions[filename] = position
+                except PermissionError:
+                    pass
+            self.positions = positions
+            time.sleep(self.sleep)
 
     def stop(self):
         ''' Stop the tailing. '''
         self.is_active = False
 
-    def check_file_validity(self, file_):
-        ''' Check whether the a given file exists, is readable and is a file. '''
-        if not os.access(file_, os.F_OK):
-            raise TailError("File '%s' does not exist" % (file_))
-        if not os.access(file_, os.R_OK):
-            raise TailError("File '%s' not readable" % (file_))
-        if os.path.isdir(file_):
-            raise TailError("File '%s' is a directory" % (file_))
 
-class TailError(Exception):
-    def __init__(self, msg):
-        self.message = msg
-    def __str__(self):
-        return self.message
+if __name__ == '__main__':
+
+    tails = []
+    sys.argv.pop(0)
+    for glob_pattern in sys.argv:
+        tail = Tail(glob_pattern)
+        tails.append(tail)
+        print('Tailing "'+ glob_pattern +'"')
+        tail.start()
+
+    if not tails:
+        print('Usage: python tail.py [glob to be tailed] [...]')
+        exit(1)
+
+    def signal_handler(signal, frame):
+        for tail in tails:
+            tail.stop()
+        print('Tailing stopped')
+        exit()
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    while True:
+        time.sleep(127)
